562c0f3e1842a3d6c19b3d3c2d6bb10e358dde72
yavelasquezo <yavelasquezo@unal.edu.co>
2022-05-05 10:32:36 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -134,7 +134,7 @@ def pregunta_03():
     pipeline = Pipeline(
         steps=[
             ("oneHotEncoder",OneHotEncoder()),
-            ("logisticRegression", LogisticRegressionCV(Cv=10)),
+            ("logisticRegression", LogisticRegressionCV(Cs=10)),
         ],
     )
 
